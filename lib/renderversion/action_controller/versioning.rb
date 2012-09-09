02f8c938cf32614734fcf8cafedddd56adc74608
Ben Willis <benjamin.willis@gmail.com>
2012-09-08 23:30:41 -0700

    exposing the requested version to the controller

--- lib/renderversion/action_controller/versioning.rb
+++ lib/renderversion/action_controller/versioning.rb
@@ -4,6 +4,8 @@ module ActionController #:nodoc:
   module Versioning
     extend ActiveSupport::Concern
 
+    attr_accessor :requested_version
+
     included do
       prepend_before_filter :set_version
     end
@@ -11,8 +13,10 @@ module ActionController #:nodoc:
     protected
       def set_version
         if request.headers.has_key?("version")
-          requested_version = request.headers["version"].to_i
-          @_lookup_context.versions = ActionView::Template::Versions.supported_versions(requested_version)
+          @requested_version = request.headers["version"].to_i
+          @_lookup_context.versions = ActionView::Template::Versions.supported_versions(@requested_version)
+        else
+          @requested_version = nil
         end
       end
   end
