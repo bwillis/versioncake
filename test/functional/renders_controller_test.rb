3dbdaf612ff99676c499a456f3e858e22382e76b
Ben Willis <benjamin.willis@gmail.com>
2012-09-12 23:07:49 -0700

    moving the rest of the controller tests over and removing the spec version

--- test/functional/renders_controller_test.rb
+++ test/functional/renders_controller_test.rb
@@ -8,6 +8,21 @@ class RendersControllerTest < ActionController::TestCase
     get :index
     assert_equal @response.body, "index.v2.html.erb"
   end
+
+  test "exposes the requested version" do
+    get :index, "api_version" => "1"
+    assert_equal @controller.requested_version, 1
+  end
+
+  test "exposes latest version when requesting the latest" do
+    get :index, "api_version" => "4"
+    assert @controller.is_latest_version
+  end
+
+  test "reports not the latest version" do
+    get :index, "api_version" => "1"
+    assert !@controller.is_latest_version
+  end
 end
 
 class ParameterStragegyTest < ActionController::TestCase
@@ -40,19 +55,19 @@ tests RendersController
     ActionView::Template::Versions.extraction_strategy = :http_header
   end
 
-  test "render version 1 of the partial based on the header API-Version" do
+  test "renders version 1 of the partial based on the header API-Version" do
     @controller.request.stubs(:headers).returns({"HTTP_API_VERSION" => "1"})
     get :index
     assert_equal @response.body, "index.v1.html.erb"
   end
 
-  test "render version 2 of the partial based on the header API-Version" do
+  test "renders version 2 of the partial based on the header API-Version" do
     @controller.request.stubs(:headers).returns({"HTTP_API_VERSION" => "2"})
     get :index
     assert_equal @response.body, "index.v2.html.erb"
   end
 
-  test "render the latest available version (v2) of the partial based on the header API-Version" do
+  test "renders the latest available version (v2) of the partial based on the header API-Version" do
     @controller.request.stubs(:headers).returns({"HTTP_API_VERSION" => "3"})
     get :index
     assert_equal @response.body, "index.v2.html.erb"
@@ -87,6 +102,44 @@ class AcceptHeaderStrategyTest < ActionController::TestCase
   test "render the latest version of the partial" do
     @controller.request.stubs(:headers).returns({"HTTP_ACCEPT" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8;api_version=abc"})
     get :index
+    assert_equal @response.body, "index.v2.html.erb"
+  end
+end
+
+class CustomStrategyTest < ActionController::TestCase
+  tests RendersController
+
+  setup do
+    ActionView::Template::Versions.extraction_strategy = lambda { |request| 2 }
+  end
+
+  test "renders version 2 of the partial based on the header Accept" do
+    get :index
+    assert_equal @response.body, "index.v2.html.erb"
+  end
+end
+
+class MultipleStrategyTest < ActionController::TestCase
+  tests RendersController
+
+  setup do
+    ActionView::Template::Versions.extraction_strategy = [:http_accept_parameter, :query_parameter]
+  end
+
+  test "renders version 1 of the partial based on the header Accept" do
+    @controller.request.stubs(:headers).returns({"HTTP_ACCEPT" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8;api_version=1"})
+    get :index
+    assert_equal @response.body, "index.v1.html.erb"
+  end
+
+  test "renders the query parameter when accept parameter isn't available" do
+    get :index, "api_version" => "1"
+    assert_equal @response.body, "index.v1.html.erb"
+  end
+
+  test "renders the higher priority accept parameter version" do
+    @controller.request.stubs(:headers).returns({"HTTP_ACCEPT" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8;api_version=2"})
+    get :index, "api_version" => "1"
     assert_equal @response.body, "index.v2.html.erb"
   end
 end
