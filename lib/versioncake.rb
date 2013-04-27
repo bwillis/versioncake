53e3a1b7fd60613ff47f72e18def802f1206817f
Ben Willis <benjamin.willis@gmail.com>
2013-04-27 14:32:32 -0700

    following cancan's pattern of explicitly stating the controller mixin as controller_additions

--- lib/versioncake.rb
+++ lib/versioncake.rb
@@ -6,9 +6,9 @@ require 'versioncake/strategies/request_parameter_strategy'
 require 'versioncake/strategies/custom_strategy'
 
 require 'versioncake/configuration'
+require 'versioncake/controller_additions'
 
 require 'versioncake/action_view/template'
 require 'versioncake/action_view/lookup_context'
 require 'versioncake/action_view/resolver'
-require 'versioncake/action_controller/versioning'
 require 'versioncake/action_view/railtie'
---
+++ lib/versioncake/controller_additions.rb
@@ -0,0 +1,32 @@
+require 'action_controller'
+
+module VersionCake
+  module ControllerAdditions
+    extend ActiveSupport::Concern
+
+    attr_accessor :requested_version, :is_latest_version, :derived_version
+
+    included do
+      prepend_before_filter :set_version
+    end
+
+    protected
+    def set_version(override_version=nil)
+      @requested_version = override_version || VersionCake::Configuration.extract_version(request)
+
+      if @requested_version.nil?
+        @derived_version = VersionCake::Configuration.latest_version
+      elsif VersionCake::Configuration.supports_version? @requested_version
+        @derived_version = @requested_version
+      elsif @requested_version > VersionCake::Configuration.latest_version
+        raise ActionController::RoutingError.new("No route match for version")
+      else
+        raise ActionController::RoutingError.new("Version is deprecated")
+      end
+      @_lookup_context.versions = VersionCake::Configuration.supported_versions(@derived_version)
+      @is_latest_version        = @derived_version == VersionCake::Configuration.latest_version
+    end
+  end
+end
+
+ActionController::Base.send(:include, VersionCake::ControllerAdditions)
