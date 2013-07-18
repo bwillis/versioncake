8a36c485b1901f7b80b9eb2d6b1711f40c49b889
Ben Willis <benjamin.willis@gmail.com>
2013-07-17 23:31:28 -0700

    Adding documentation to the controller additions

--- lib/versioncake/controller_additions.rb
+++ lib/versioncake/controller_additions.rb
@@ -4,13 +4,31 @@ module VersionCake
   module ControllerAdditions
     extend ActiveSupport::Concern
 
-    attr_accessor :requested_version, :is_latest_version, :derived_version
+    # The explicit version requested by a client, this may not
+    # be the rendered version and may also be nil.
+    attr_accessor :requested_version
 
+    # A boolean check to determine if the latest version is requested.
+    attr_accessor :is_latest_version
+
+    # The requested version by a client or if it's nil the latest or default
+    # version configured.
+    attr_accessor :derived_version
+
+    # set_version is the prepend filter that will determine the version of the
+    # requests.
     included do
       prepend_before_filter :set_version
     end
 
     protected
+
+    # Sets the version of the request as well as several accessor variables.
+    #
+    # @param override_version a version number to use instead of the one extracted
+    #     from the request
+    #
+    # @return No explicit return, but several attributes are exposed
     def set_version(override_version=nil)
       versioned_request         = VersionCake::VersionedRequest.new(request, override_version)
       @requested_version        = versioned_request.extracted_version
