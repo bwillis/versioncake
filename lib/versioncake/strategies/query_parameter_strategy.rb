81d1bfef4aad4987dcca6e57e7c9540898c121c3
Michael Elfassy <michael@smashingboxes.com>
2013-10-29 11:07:09 -0400

    user params instead of query_parameters for nicer route paths

--- lib/versioncake/strategies/query_parameter_strategy.rb
+++ lib/versioncake/strategies/query_parameter_strategy.rb
@@ -2,8 +2,8 @@ module VersionCake
   class QueryParameterStrategy < ExtractionStrategy
 
     def execute(request)
-      if request.query_parameters.key? @@version_string.to_sym
-        request.query_parameters[@@version_string.to_sym]
+      if request.params.key? @@version_string.to_sym
+        request.params[@@version_string.to_sym]
       end
     end
 
