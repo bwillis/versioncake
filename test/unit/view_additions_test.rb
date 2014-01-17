15fbe1f944ce61b11c432747f9488db9ea379af7
Ben Willis <benjamin.willis@gmail.com>
2014-01-16 23:09:35 -0800

    Fixing asset_equal parameter ordering
    Expectation goes first, result second.

--- test/unit/view_additions_test.rb
+++ test/unit/view_additions_test.rb
@@ -7,21 +7,21 @@ class ViewAdditionsTest < ActiveSupport::TestCase
 
   test "it retrieves the correct handler and format when only hanlder and format are present" do
     handler, format = @resolver.extract_handler_and_format('application.html.erb', nil)
-    assert_equal format.to_s, 'text/html'
+    assert_equal 'text/html', format.to_s
   end
 
   test "it retrieves the correct handler and format when only hanlder, format and version are present" do
     handler, format = @resolver.extract_handler_and_format('application.json.v1.jbuilder', nil)
-    assert_equal format.to_s, 'application/json'
+    assert_equal 'application/json', format.to_s
   end
 
   test "it retrieves the correct handler and format when only hanlder, format and locale are present" do
     handler, format = @resolver.extract_handler_and_format('application.en.json.jbuilder', nil)
-    assert_equal format.to_s, 'application/json'
+    assert_equal 'application/json', format.to_s
   end
 
   test "it retrieves the correct handler and format when only hanlder, format, locale and version are present" do
     handler, format = @resolver.extract_handler_and_format('application.en.json.v1.jbuilder', nil)
-    assert_equal format.to_s, 'application/json'
+    assert_equal 'application/json', format.to_s
   end
 end
