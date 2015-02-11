0efa4a767164dc03d2cf9c9171031123c3471f48
Ben <benjamin.willis@gmail.com>
2015-02-10 22:09:51 -0800

    Adding pending tests that need to be written

---
+++ spec/unit/version_context_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe VersionCake::VersionContext do
+
+  describe '#supported_versions' do
+    xit {}
+  end
+
+  describe '#is_latest_version?' do
+    xit {}
+  end
+end
