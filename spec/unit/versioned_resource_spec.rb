3b434da750ade42b968468b9a249ee38f072e7cb
Ben <benjamin.willis@gmail.com>
2015-03-08 20:43:16 -0700

    adding coverage to VersionedResource

--- spec/unit/versioned_resource_spec.rb
+++ spec/unit/versioned_resource_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 describe VersionCake::VersionedResource do
 
-  describe '#latest_version' do
-    xit {}
-  end
+  let(:resource) { described_class.new('hello/123', [1,2], [3], [4,5,6])}
 
+  describe '#latest_version' do
+    subject { resource.latest_version }
+
+    it { is_expected.to eq 6 }
+  end
 end
